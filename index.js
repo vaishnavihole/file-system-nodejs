716ada0955368138ddc9c44ddab258a9ae615495
vaishnavihole <vaishnavihole1@gmail.com>
2023-04-02 13:11:39 +0530

    add extra data by using appendFileSync

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const fs = require("fs");
 // Creating a new file
 //fs.writeFileSync('read.txt', "Good Morning")
 
-fs.writeFileSync('read.txt', "Hey,Good Morning")
+//fs.writeFileSync('read.txt', "Hey,Good Morning")
+
+//add text in existing file
+fs.appendFileSync('read.txt', " how are you I am fine Thank You");
