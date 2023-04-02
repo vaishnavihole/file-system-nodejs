f44dfa38f36843e947f76c2a25761e5394231691
vaishnavihole <vaishnavihole1@gmail.com>
2023-04-02 13:19:33 +0530

    use buffer data type

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ const fs = require("fs");
 //fs.writeFileSync('read.txt', "Hey,Good Morning")
 
 //add text in existing file
-fs.appendFileSync('read.txt', " how are you I am fine Thank You");
+// fs.appendFileSync('read.txt', " how are you I am fine Thank You");
+
+//read data in another file
+
+//Node.js includes an additional data type calles buffer.
+// Not available in js.
+// Buffer is mainely used to store binary data,
+// while reading from file or receiving packets over the network.
+
+const buf_data = fs.readFileSync('read.txt');
+console.log(buf_data);
